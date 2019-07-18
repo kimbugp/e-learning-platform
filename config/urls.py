46d30349b221f823a2648cd93df1058196713c86
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-18 11:03:57 +0300

    name user routes

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('users/', include(('app.authentication.urls', 'user'), namespace='user'),),
+    path('users/', include(('app.authentication.urls','user')),),
     path('users/', include('django.contrib.auth.urls')),
     path('courses/', include('app.courses.urls')),
 
