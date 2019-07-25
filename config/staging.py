7ea7bd7f2df2f664f0222545d22cdb465786f38a
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-25 18:58:15 +0300

    deploy to heroku

--- config/staging.py
+++ config/staging.py
@@ -5,4 +5,4 @@ import django_heroku
 django_heroku.settings(locals(), test_runner=False)
 DEBUG = False
 
-ALLOWED_HOSTS += ['.herokuapp.com']  # noqa
+# ALLOWED_HOSTS += ('.herokuapp.com', 'mirest.herokuapp.com',)  # noqa
