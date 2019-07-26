4e7f7e8e589b2d0bde337c8c9b0741e2a4866e8e
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-26 15:07:14 +0300

    fix static files deployment

--- config/default.py
+++ config/default.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ORIGIN_WHITELIST = (
--- config/staging.py
+++ config/staging.py
@@ -5,4 +5,13 @@ import django_heroku
 django_heroku.settings(locals(), test_runner=False)
 DEBUG = False
 
-# ALLOWED_HOSTS += ('.herokuapp.com', 'mirest.herokuapp.com',)  # noqa
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
