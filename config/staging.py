041f111050519ea9788548df5b206385dd086020
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2019-07-26 20:41:36 +0300

    fix deployment

--- config/staging.py
+++ config/staging.py
@@ -4,14 +4,3 @@ import django_heroku
 
 django_heroku.settings(locals(), test_runner=False)
 DEBUG = False
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
